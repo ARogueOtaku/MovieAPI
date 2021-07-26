95d964afb2fef654681f8d71b59c737cc2a89102
AntiPrim3 <amit.rockinad.das@gmail.com>
2021-07-26 23:05:28 +0530

    Added Request Validator Method

--- api/listener.js
+++ api/listener.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const fetch = require("node-fetch");
 
+//Get TMDB API URL from Request Request URL
 const getTMDBUrl = (netlifyEvent) => {
   const path = netlifyEvent.path.substring(29);
   if (typeof path !== "string" || path.length < 1) throw "No Path Specified!";
@@ -9,22 +10,45 @@ const getTMDBUrl = (netlifyEvent) => {
   return fullPath;
 };
 
+//Validate Request for HTTP Type and CORS
 const validateRequest = (netlifyEvent) => {
-  if (netlifyEvent.httpMethod !== "GET") throw "Only GET Requests Allowed";
-  const allowedOrigins = process.env.allowedOrigins.split(process.env.delimeter || ";");
-  if (!process.env.allowedOrigins.includes(netlifyEvent.headers.host)) throw "Uauthorized";
+  const validationResult = {
+    err: undefined,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  };
+  //Validate HTTP Method
+  if (netlifyEvent.httpMethod !== "GET") {
+    validationResult["err"] = "Only GET Allowed!";
+    return validationResult;
+  }
+
+  //Validate Request Origin
+  const origin = netlifyEvent.headers.origin;
+  if (origin && typeof origin === "string") {
+    const allowedOrigins = process.env.allowedOrigins.split(process.env.delimeter || ";");
+    if (!allowedOrigins.includes(origin)) {
+      validationResult["err"] = "Uauthorized";
+      validationResult["headers"]["Access-Control-Allow-Origin"] = "";
+      return validationResult;
+    } else headers["Access-Control-Allow-Origin"] = origin;
+  }
+  return validationResult;
 };
 
+//API Request Handler
 exports.handler = async (evt) => {
-  let headers = { "Access-Control-Allow-Origin": "*" };
+  const valResult = validateRequest(evt);
+  console.log(evt);
   try {
-    validateRequest(evt);
+    if (valResult["err"]) throw valResult["err"];
     const API_URL = getTMDBUrl(evt);
     const response = await fetch(API_URL);
     const data = await response.json();
-    return { statusCode: 200, headers, body: JSON.stringify(data) };
+    return { statusCode: 200, headers: valResult["headers"], body: JSON.stringify(data) };
   } catch (err) {
     console.log(err);
-    return { statusCode: 400, headers, body: err.toString() };
+    return { statusCode: 400, headers: valResult["headers"], body: err.toString() };
   }
 };
